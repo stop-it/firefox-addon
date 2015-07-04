ad47052721ec0d7747a2cbe9c5d02246a44ee262
ondrejd <ondrejd@gmail.com>
2015-07-04 13:17:45 +0200

    Added blocking URL (just rewrite of original solution).

--- index.js
+++ index.js
@@ -5,10 +5,12 @@
 let { Cc, Ci, Cu, Cr } = require('chrome');
 let self = require('sdk/self');
 let tabs = require('sdk/tabs');
+let events = require('sdk/system/events');
 
-Cu.import('resource://gre/modules/Services.jsm');
 Cu.import('resource://gre/modules/Downloads.jsm');
 Cu.import('resource://gre/modules/osfile.jsm');
+Cu.import('resource://gre/modules/Services.jsm');
+Cu.import('resource://gre/modules/Sqlite.jsm');
 Cu.import('resource://gre/modules/Task.jsm');
 
 /**
@@ -21,7 +23,7 @@ const DB_DOWNLOAD_URL = 'http://ondrejd.savana-hosting.cz/stop-it/db.sqlite';
  * Holds path to the local copy of Stop-It database.
  * @const {String}
  */
-const DB_FILE_PATH = OS.Path.join(OS.Constants.Path.profileDir, 'db.sqlite');
+const DB_FILE_PATH = OS.Path.join(OS.Constants.Path.profileDir, 'stop-it.sqlite');
 
 /**
  * URL of our download page.
@@ -30,49 +32,19 @@ const DB_FILE_PATH = OS.Path.join(OS.Constants.Path.profileDir, 'db.sqlite');
 const ADDON_PAGE_URL = self.data.url('download-page.html');
 
 /**
- * Prototype object for our listener.
+ * String with our blocked URL addresses.
+ * @var {String} Urls
  */
-function HttpOnModifyRequestListenerPrototype() {}
-HttpOnModifyRequestListenerPrototype.prototype = {
-	/**
-	 * Observe for "http-on-modify-request" event.
-	 * @param {Object} aSubject
-	 * @param {String} aTopic
-	 * @param {Object} aData
-	 */
-	observe: function(aSubject, aTopic, aData) {
-		console.log('onHttpOnModifyRequestevent');
-		console.log(aSubject);
-		console.log(aTopic);
-		console.log(aData);
-	
-		let httpChannel = aSubject.QueryInterface(Ci.nsIHttpChannel);
-		let requestUrl = httpChannel.URI.spec;
-	
-		// TODO Get URLs from the database (if it is downloaded)
-		let urls = [];
-		for (let i=0; i<urls.length; i++) {
-			if (requestUrl.indexOf(urls[i]) > -1) {
-				// This aborts the load:
-				//httpChannel.cancel(Cr.NS_BINDING_ABORTED);
-				// This replace contents which browser is loading:
-				httpChannel.redirectTo(
-					Services.io.newURI(
-						'data:text,ILLEGAL_&_DANGEROUS_CONTENT_BLOCKED', 
-						null, 
-						null
-					)
-				);
-				break;
-			}
-		}
-	} // end observe(aSubject, aTopic, aData)
-}; // End of HttpOnModifyRequestListenerPrototype.prototype
+let Urls = '';
+
+/**
+ * Indicates if blocked URL addresses are already loaded.
+ * @var {Boolean} UrlsAreInitialized
+ */
+let UrlsAreInitialized = false;
 
 /**
  * Download Stop-It database at the first run.
- * @see https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/Downloads.jsm
- * @todo Show user message with result of the operation!
  * @param {Worker} aWorker
  */
 function downloadDatabase(aWorker) {
@@ -83,6 +55,7 @@ function downloadDatabase(aWorker) {
 			onDownloadChanged: function (aDownload) {
 				if (aDownload.succeeded === true) {
 					aWorker.port.emit('download_succeeded');
+					loadBlockedUrls();
 				} else {
 					let downloadInfo = {
 						currentBytes: aDownload.currentBytes,
@@ -104,39 +77,20 @@ function downloadDatabase(aWorker) {
 		let download = yield Downloads.createDownload(options);
 		list.add(download);
 
-		download.start();
+		download.start().then(function () {
+				list.remove(download);
+				list.removeView(view); 
+		});
+
 		aWorker.port.emit('download_started');
-	}).then(null, Cu.reportError);
-} // end downloadDatabase()
-
-/**
- * Remove Stop-It database from the file system.
- * @todo Show user message with result of the operation!
- */
-function removeDatabase() {
-	try {
-		OS.File.remove(DB_DOWNLOAD_URL);
-	} catch (exception) {
-		if (exception instanceof OS.File.Error && exception.becauseNoSuchFile) {
-			// The file does not exist
-			console.log('Stop-It database can not be removed - file does not exist!');
-			console.log(DB_DOWNLOAD_URL);
+	}).then(
+		null,
+		function (error) {
+			console.log(error);
+			aWorker.port.emit('download_errored');
 		}
-	}
-	// TODO Try also `Task` approach!
-	/*Task.spawn(function () {
-		yield OS.File.remove(DB_DOWNLOAD_URL);
-
-		console.log('Stop-It database has been removed.');
-	}).then(null, function (aError) {
-		console.log(aError);
-		if (aError instanceof OS.File.Error && aError.becauseNoSuchFile) {
-			// The file does not exist
-			console.log('Stop-It database can not be removed - file does not exist!');
-			console.log(DB_DOWNLOAD_URL);
-		}
-	});*/
-} // end removeDatabase()
+	);
+} // end downloadDatabase(aWorker)
 
 /**
  * Show page that informs about downloading of Stop-It database.
@@ -165,17 +119,98 @@ function showDownloadPage() {
 	});
 } // end showDownloadPage()
 
-// Check if exists Stop-It database and download it if not
-if (OS.File.exists(DB_FILE_PATH) !== true) {
-	showDownloadPage();
-}
+/**
+ * Load all blocked URL addresses from the database to one big text.
+ */
+function loadBlockedUrls() {
+	Task.spawn(
+		function* loadUrls() {
+			let conn = yield Sqlite.openConnection({ path: DB_FILE_PATH }); 
+	
+			try {
+				let result = yield conn.execute(
+					'SELECT group_concat(trim(Url)) FROM Urls ORDER BY Url ASC '
+				);
+
+				Urls = result[0].getResultByIndex(0);
+			} catch(e) {
+				console.log(e.message);
+			} finally {
+				yield conn.close();
+			}
+		}
+	).then(
+		function () {
+			if (Urls !== '') {
+				console.log('Blocked URL addresses are successfully loaded!');
+				UrlsAreInitialized = true;
+			}
+		}
+	);
+} // end loadBlockedUrls()
+
+/**
+ * Listener for HTTP requests - this is what blocking the bad URLs.
+ * @param {Event} aEvent
+ */
+function onHttpOnModifyRequestListener(aEvent) {
+	if (UrlsAreInitialized !== true) {
+		//console.log('Blocked URL addresses are not loaded yet!');
+		return;
+	}
+
+	let httpChannel = aEvent.subject.QueryInterface(Ci.nsIHttpChannel);
+	let referrerUrlHost = (httpChannel.referrer) ? httpChannel.referrer.host : '';
+	let requestUrlHost = httpChannel.URI.host;
+
+	//console.log('Investigating loading of: "' + requestUrlHost + '", referrer: "' + referrerUrlHost + '"');
+
+	// Test if URL should be blocked
+	if (Urls.indexOf(requestUrlHost) >= 0) {
+		//console.log('URL "' + requestUrlHost + '" is blocked!');
+
+		// This aborts the load:
+		//httpChannel.cancel(Cr.NS_BINDING_ABORTED);
+
+		// This replace contents which browser is loading:
+		httpChannel.redirectTo(
+			Services.io.newURI(
+				'data:text,ILLEGAL_&_DANGEROUS_CONTENT_BLOCKED', 
+				null, 
+				null
+			)
+		);
+	}
+
+	//console.log('URL is not blocked.');
+} // end onHttpOnModifyRequestListener(aEvent)
+
+/**
+ * Listener for application quit.
+ * @param {Event} aEvent
+ */
+function onApplicationQuitListener(aEvent) {
+	// Remove our listeners
+	events.off('quit-application', onApplicationQuitListener);
+	events.off('http-on-modify-request', onHttpOnModifyRequestListener);
+} // end onApplicationQuitListener(aEvent)
 
 // ==========================================================================
-// Generated by JPM:
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-//function dummy(text, callback) {
-//	callback(text);
-//}
-//
-//exports.dummy = dummy;
+// Here is "start-up" code:
+
+// Main event listeners
+events.on('http-on-modify-request', onHttpOnModifyRequestListener);
+events.on('quit-application', onApplicationQuitListener);
+
+// TODO This would be better after 'app-startup' event or something like this,
+//      but there is no proper one.
+//      See: https://developer.mozilla.org/en-US/docs/Observer_Notifications
+
+// Check if Stop-It database exists
+if (OS.File.exists(DB_FILE_PATH) !== true) {
+	// And start download if not
+	showDownloadPage();
+} else {
+	// Load all URLs at once and store them as a text!
+	loadBlockedUrls();
+}
